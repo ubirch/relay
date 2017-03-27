eb98ddce6cd4036d031b857326a653f3eaf765e1
Niranjan Rao <niranjanhrao@gmail.com>
2017-03-27 11:02:57 +0200

    wake up from VLLS0 mode using ext pin as the LLWU this works

--- source/main.cpp
+++ source/main.cpp
@@ -76,29 +76,11 @@ static const char *const message_template = "POST /api/avatarService/v1/device/u
 #define BOARD_WAKEUP_IRQ PORTC_IRQn
 
 /*!
-* @brief ISR for Alarm interrupt
-*
-*/
-void RTC_IRQHandler(void)
-{
-    if (RTC_GetStatusFlags(RTC) & kRTC_AlarmFlag)
-    {
-        /* Clear alarm flag */
-        RTC_ClearStatusFlags(RTC, kRTC_AlarmInterruptEnable);
-    }
-}
-
-/*!
  * @brief LLWU interrupt handler.
  */
 
 void LLWU_IRQHandler(void)
 {
-    if (RTC_GetStatusFlags(RTC) & kRTC_AlarmFlag)
-    {
-        RTC_ClearStatusFlags(RTC, kRTC_AlarmInterruptEnable);
-    }
-
     /* If wakeup by external pin. */
     if (LLWU_GetExternalWakeupPinFlag(LLWU, LLWU_WAKEUP_PIN_IDX))
     {
@@ -159,70 +141,20 @@ void ShowPowerMode(smc_power_state_t powerMode)
     }
 }
 
-void initrtc(void) {
-    /* Init RTC */
-    /*
-     * rtcConfig.wakeupSelect = false;
-     * rtcConfig.updateMode = false;
-     * rtcConfig.supervisorAccess = false;
-     * rtcConfig.compensationInterval = 0;
-     * rtcConfig.compensationTime = 0;
-     */
-    rtc_datetime_t date;
-    rtc_config_t rtcConfig;
-
-    RTC_GetDefaultConfig(&rtcConfig);
-    RTC_Init(RTC, &rtcConfig);
-    /* Select RTC clock source */
-    /* Enable the RTC 32KHz oscillator */
-    RTC->CR |= RTC_CR_OSCE_MASK;
-
-    printf("RTC example: set up time to wake up an alarm\r\n");
-
-    /* Set a start date time and start RT */
-    date.year   = 2017U;
-    date.month  = 03U;
-    date.day    = 22U;
-    date.hour   = 14U;
-    date.minute = 36;
-    date.second = 40;
-
-    /* RTC time counter has to be stopped before setting the date & time in the TSR register */
-    RTC_StopTimer(RTC);
-    /* Set RTC time to default */
-    RTC_SetDatetime(RTC, &date);
-    /* Get RTC time */
-    RTC_GetDatetime(RTC, &date);
-
-    /* print default time */
-    printf("Current datetime: %04hd-%02hd-%02hd %02hd:%02hd:%02hd\r\n",
-           date.year,
-           date.month,
-           date.day,
-           date.hour,
-           date.minute,
-           date.second);
-
-    /* Enable RTC alarm interrupt */
-    RTC_EnableInterrupts(RTC, kRTC_AlarmInterruptEnable);
-
-    /* Enable at the NVIC */
-//    EnableIRQ(RTC_IRQn);
-    NVIC_EnableIRQ(RTC_IRQn);
-}
-
-volatile uint32_t milliseconds = 0;
-bool on = true;
-
-void SysTick_Handler() {
-    milliseconds++;
-    if (milliseconds % 1000 == 0) on = !on;
-    led = 1;
-}
+//volatile uint32_t milliseconds = 0;
+//bool on = true;
+//
+//void SysTick_Handler() {
+//    milliseconds++;
+//    if (milliseconds % 1000 == 0) on = !on;
+//    led = 1;
+//}
 
 int main(void) {
     uint32_t sec;
     uint32_t currSeconds;
+    uint32_t freq = 0;
+    smc_power_state_t curPowerState;
 
     printf("############the power modes ##########\r\n");
     SMC_SetPowerModeProtection(SMC, kSMC_AllowPowerModeAll);
@@ -231,9 +163,6 @@ int main(void) {
         PMC_ClearPeriphIOIsolationFlag(PMC);
         NVIC_ClearPendingIRQ(LLWU_IRQn);
     }
-
-    uint32_t freq = 0;
-    smc_power_state_t curPowerState;
 
 //     print message on cold boot
     if (kRCM_SourceWakeup & RCM_GetPreviousResetSources(RCM)) /* Wakeup from VLLS. */
@@ -248,30 +177,14 @@ int main(void) {
     ShowPowerMode(curPowerState);
     printf("    Core Clock = %dHz \r\n", freq);
 
-    wait(7);
-//    NVIC_DisableIRQ(SysTick_IRQn);
+    wait(4);
 
-    initrtc();
+//    NVIC_DisableIRQ(SysTick_IRQn);
 
     NVIC_SetVector(LLWU_IRQn, (uint32_t)&LLWU_IRQHandler);
     NVIC_EnableIRQ(LLWU_IRQn);
-    /* Start the RTC time counter */
-    RTC_StartTimer(RTC);
-//    NVIC_EnableIRQ(SysTick_IRQn);
 
-    /*++++++++++++++++++++++++++++++++++++++++*/
-    /* Set Alaram in seconds */
-    sec = 10;
-    /* Read the RTC seconds register to get current time in seconds */
-    currSeconds = RTC->TSR;
-    /* Add alarm seconds to current time */
-    currSeconds += sec;
-    /* Set alarm time in seconds */
-    RTC->TAR = currSeconds;
-    /*++++++++++++++++++++++++++++++++++++++++*/
-
-    LLWU_EnableInternalModuleInterruptWakup(LLWU, 5U, true);
-//    LLWU_SetExternalWakeupPinMode(LLWU, LLWU_WAKEUP_PIN_IDX, LLWU_WAKEUP_PIN_TYPE);
+    LLWU_SetExternalWakeupPinMode(LLWU, LLWU_WAKEUP_PIN_IDX, LLWU_WAKEUP_PIN_TYPE);
 
     //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
     printf("now let me sleep2\r\n\n\n");
